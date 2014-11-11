120c4e4c802dd6bc0aa8145bf3d680df7acdfaa9
Simon Wittber <simonwittber@gmail.com>
2014-11-11 21:22:27 +0800

    re-arranging module / scripts.

---
+++ netwrokserver/components/mailqueue.py
@@ -0,0 +1,23 @@
+import asyncio
+import aiopg
+import hashlib
+from lib import nwdb
+from __main__ import handler
+
+@asyncio.coroutine
+def send(client, email, subject, body):
+    with (yield from nwdb.connection()) as conn:
+        member_id = None
+        if "member_id" in client.session:
+            member_id = client.session["member_id"]
+        cursor = yield from conn.cursor()
+        yield from cursor.execute("""
+        insert into mailqueue(member_id, address, subject, body)
+        select %s, %s, %s, %s
+        returning id
+        """, [member_id, email, subject, body])
+        rs = yield from cursor.fetchone()
+        return rs[0]
+
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name = "netwrokserver",
+    version = "0.0.1",
+    author = "Simon Wittber",
+    author_email = "simonwittber@differentmethods.com",
+    packages=['netwrokserver'],
+)
