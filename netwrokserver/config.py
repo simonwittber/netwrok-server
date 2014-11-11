ab343884b153ccb52d004d9263c6fd5d9b4b3f85
Simon Wittber <simonwittber@gmail.com>
2014-11-11 22:28:16 +0800

    still shufflin...

--- netwrokserver/config.py
+++
@@ -1,6 +0,0 @@
-#Start the mailer process when server starts
-START_MAILER = True
-#seconds to wait before checking for new messages in queue.
-MAILER_IDLE_TIME = 30
-#from address used when system sends email.
-FROM_ADDRESS = "noreply@differentmethods.com"
