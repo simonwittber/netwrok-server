c10a9e3084cdaaf11602eb57b10ba4d83898751b
Simon Wittber <simonwittber@gmail.com>
2014-12-06 22:58:41 +0800

    added squad level shared objects

--- src/netwrok/squad.py
+++ src/netwrok/squad.py
@@ -9,20 +9,24 @@ from . import room
 
 @core.handler
 def send(client, msg, *args):
+    client.require_auth()
     if client.squad is not None:
         client.squad.send(msg, client.member_id, *args)
     
 
 @core.handler
 def invite(client, member_id):
+    client.require_auth()
     if client.squad is None:
-        client.squad = room.Room("Squad " + str(client.member_id))
+        squad = client.squad = room.Room("Squad " + str(client.member_id))
+        squad.objects = {}
         client.join(squad)
     client.whisper(member_id, "squad.invite", client.member_info, client.squad.name)
 
 
 @core.handler
 def join(client, member_id):
+    client.require_auth()
     if client.squad is not None:
         yield from client.leave(squad)
     host = client.clients[member_id]
@@ -32,9 +36,26 @@ def join(client, member_id):
 
 @core.handler
 def leave(client):
+    client.require_auth()
     if client.squad is not None:
         yield from client.leave(client.squad)
         client.squad = None
 
 
-    
+@core.handler
+def set_object(client, key, value):
+    client.require_auth()
+    client.squad.objects[key] = value
+
+
+@core.function
+def get_object(client, key):
+    client.require_auth()
+    return client.squad.objects.get(key, None)
+
+
+@core.function
+def get_object_keys(client):
+    client.require_auth()
+    return list(client.squad.objects.keys())
+
