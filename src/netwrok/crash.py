2501da2836cb19138a58eb9f9c72668bd73fee8b
Simon Wittber <simonwittber@gmail.com>
2015-04-28 15:33:42 +0800

    crash reports!

---
+++ src/netwrok/crash.py
@@ -0,0 +1,24 @@
+import asyncio
+import aiopg
+
+from . import core
+from . import nwdb
+from . import presence
+
+
+@core.handler
+def submit(client, crash_report):
+    """
+    Add a crash report.
+    """
+    client.require_auth()
+    with (yield from nwdb.connection()) as conn:
+        cursor = yield from conn.cursor()
+        yield from cursor.execute("""
+        insert into crashreport(member_id, report)
+        select %s, %s
+        returning id
+        """, [client.session["member_id"], crash_report])
+        rs = yield from cursor.fetchone()
+        yield from client.send("crash.submit", [rs[0]])
+
